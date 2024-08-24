5d10c51a3ab41aba7ead9c13c5f896761dda3f5b
Bányász Tamás <tamasbanyasz@gmail.com>
2024-08-24 13:26:50 +0200

    Finished the second page

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"image/color"
 	"log"
+	"math/rand"
 	"os"
 	"time"
 
 	"gioui.org/app"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/unit"
 	"gioui.org/widget"
@@ -61,6 +64,10 @@ func run(window *app.Window) error {
 	buttonClicked := false
 	var userInputs []userInput
 
+	rand.Seed(time.Now().UnixNano())
+	randomNumber := rand.Float32()
+	backgroundRect := image.Rect(0, 0, 800, 50)
+
 	for {
 		switch e := window.Event().(type) {
 		case app.DestroyEvent:
@@ -71,6 +78,7 @@ func run(window *app.Window) error {
 			window.Option(app.Size(fixedWidth, fixedHeight))
 
 			gtx := app.NewContext(&ops, e)
+			paint.Fill(gtx.Ops, color.NRGBA{R: 0, G: 240, B: 240, A: 255})
 
 			if turnPage.Clicked(gtx) {
 				if page == 0 {
@@ -82,24 +90,6 @@ func run(window *app.Window) error {
 				}
 				fmt.Println("Page turned!")
 			}
-			layout.Stack{}.Layout(gtx,
-				layout.Expanded(func(gtx layout.Context) layout.Dimensions {
-					paint.Fill(gtx.Ops, color.NRGBA{R: 0, G: 237, B: 245, A: 255})
-					return layout.Dimensions{Size: gtx.Constraints.Max}
-				}),
-				layout.Stacked(func(gtx layout.Context) layout.Dimensions {
-					return layout.Inset{
-						Top:   unit.Dp(430),
-						Left:  unit.Dp(520),
-						Right: unit.Dp(10),
-					}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-						btn := material.Button(theme, &turnPage, pageButtonText)
-						btn.Background = color.NRGBA{R: 33, G: 80, B: 255, A: 255}
-
-						return btn.Layout(gtx)
-					})
-				}),
-			)
 
 			switch page {
 			case 0:
@@ -119,6 +109,7 @@ func run(window *app.Window) error {
 					fmt.Println("Button pressed!")
 					input.SetText("")
 					buttonClicked = true
+
 				}
 
 				if buttonClicked && text != "" {
@@ -177,27 +168,78 @@ func run(window *app.Window) error {
 						Left:  unit.Dp(50),
 						Right: unit.Dp(50),
 					}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-						return material.Label(theme, unit.Sp(slider.Value*100), text).Layout(gtx)
+						m := material.Label(theme, unit.Sp(float32(slider.Value*100)), text)
+
+						if float32(slider.Value) > float32(randomNumber) && float32(slider.Value) < float32(randomNumber)+0.1 {
+							m.Color = color.NRGBA{R: 250, G: 0, B: 0, A: 255}
+
+						}
+						return m.Layout(gtx)
+
 					})
+
 				})
 
 			case 1:
 
-				layout.Flex{
-					Axis:    layout.Vertical,
-					Spacing: layout.SpaceEvenly,
-				}.Layout(gtx,
+				layout.Flex{}.Layout(gtx,
+					layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+						return material.List(theme, &list).Layout(gtx, len(userInputs), func(gtx layout.Context, i int) layout.Dimensions {
+							texts := userInputs[i]
 
-					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-						return list.Layout(gtx, len(userInputs), func(gtx layout.Context, index int) layout.Dimensions {
-							texts := userInputs[index]
-							return material.Body1(theme, fmt.Sprintf("Input was: %s\nDate: %s", texts.input,
-								texts.addedTime)).Layout(gtx)
+							margins := layout.Inset{
+								Top:    unit.Dp(30),
+								Bottom: unit.Dp(15),
+								Left:   unit.Dp(20),
+								Right:  unit.Dp(30),
+							}
+
+							return margins.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+								return layout.UniformInset(unit.Dp(8)).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+									return material.Body1(theme, fmt.Sprintf(" Input was: %s\n Date: %s", texts.input, texts.addedTime.Format("2006-01-02 15:04:05"))).Layout(gtx)
+								})
+							})
 						})
 					}),
 				)
+
 			}
 
+			layout.Stack{}.Layout(gtx,
+				layout.Expanded(func(gtx layout.Context) layout.Dimensions {
+					backgroundColor := color.NRGBA{R: 33, G: 80, B: 255, A: 255}
+
+					defer clip.Rect(backgroundRect).Push(gtx.Ops).Pop()
+					paint.ColorOp{Color: backgroundColor}.Add(gtx.Ops)
+					paint.PaintOp{}.Add(gtx.Ops)
+
+					return layout.Dimensions{
+						Size: gtx.Constraints.Max,
+					}
+
+				}),
+				layout.Stacked(func(gtx layout.Context) layout.Dimensions {
+					return layout.Inset{
+						Top:    unit.Dp(10),
+						Bottom: unit.Dp(20),
+						Left:   unit.Dp(20),
+						Right:  unit.Dp(20),
+					}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+						return material.Body1(theme, "Background Content").Layout(gtx)
+					})
+				}),
+				layout.Stacked(func(gtx layout.Context) layout.Dimensions {
+					return layout.Inset{
+						Top:   unit.Dp(430),
+						Left:  unit.Dp(520),
+						Right: unit.Dp(10),
+					}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+						btn := material.Button(theme, &turnPage, pageButtonText)
+						btn.Background = color.NRGBA{R: 33, G: 80, B: 255, A: 255}
+						return btn.Layout(gtx)
+					})
+				}),
+			)
 			e.Frame(gtx.Ops)
 		}
 	}
